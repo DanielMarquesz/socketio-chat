9160be78d95c8f092f526c7190ea404c07157f53
Daniel Marques <daniel.marques142@gmail.com>
2021-03-11 00:03:42 -0300

    refactor: add field name, username event listeners backend

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
+var jooj;
+
 io.on("connection",(socket) =>{ // socket: References the client that is connected
+
+  socket.on("connect", () =>{
+    console.log(socket.id + " Was Connected, YEY!")
+  }) 
 
   socket.on("disconnect", () =>{
     console.log(socket.id + " Was disconnected!")
   }) 
+
+  socket.on("data", (data) => {
+    jooj = data
+    console.log(data)
+  })
 })
 
 app.set("view engine","ejs")
@@ -19,3 +30,5 @@ app.get("/", (req, res) => {
 http.listen(3000, () => {
   console.log('The app is Running!')
 })
+
+module.exports = jooj
